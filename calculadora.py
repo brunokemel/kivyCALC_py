cb45f8a17250beec26ce3ac120e1f0dbd76c5dc2
Bruno Kemel <br.kemel@gmail.com>
2025-08-15 16:45:50 -0300

    finalizado calc Pyhton

--- calculadora.py
+++ calculadora.py
@@ -5,18 +5,19 @@ from kivy.uix.textinput import TextInput
 
 class MainApp(App):
     def build(self):
-        self.operatos = ["/", "*", "+", "-"]
+        self.operators = ["/", "*", "+", "-"]
         self.last_was_operator = None
         self.last_button = None
 
         main_layout = BoxLayout(orientation="vertical")
+
         self.solution = TextInput(
             multiline=False,
             readonly=True,
-            haling="right",
+            halign="right",
             font_size=55
         )
-        main_layout.add.widget(self.solution)
+        main_layout.add_widget(self.solution)
 
         buttons = [
             ["7", "8", "9", "/"],
@@ -29,16 +30,48 @@ class MainApp(App):
             h_layout = BoxLayout()
             for label in row:
                 button = Button(
-                    tex=label,
-                    pos_hint={"center_x: 0.5, center_y: 0.5"},
+                    text=label,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
                 )
-                
-                equals_button = Button(
-                    text="=",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5}
-                )
-                equals_button.bind(on_press=self.on_solution)
-                main_layout.add_widget(equals_button)
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
 
-                return main_layout
-            print("Conta ValidationError")
+        equals_button = Button(
+            text="=",
+            pos_hint={"center_x": 0.5, "center_y": 0.5}
+        )
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
+
+        return main_layout
+
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+
+        if button_text == "C":
+            self.solution.text = ""
+        else:
+            if current and (self.last_was_operator and button_text in self.operators):
+                return
+            elif current == "" and button_text in self.operators:
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+
+        self.last_button = button_text
+        self.last_was_operator = button_text in self.operators
+
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            try:
+                solution = str(eval(text))
+                self.solution.text = solution
+            except Exception:
+                self.solution.text = "Erro"
+
+if __name__ == '__main__':
+    MainApp().run()
